14476719a148acb2a6a5973193f16c0545c24535
devilman97 <118475791+devilman97@users.noreply.github.com>
2023-05-11 20:56:18 +0200

    Add files via upload

--- pages/FormPage.js
+++ pages/FormPage.js
@@ -1,12 +1,71 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import axios from 'axios';
 
 const FormPage = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Form Page</Text>
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegister = async () => {
+    if (password !== confirmPassword) {
+      Alert.alert('Error', 'Passwords do not match');
+      return;
+    }
+    try {
+      const response = await axios.get(`STRAPI ?email=${email}`);
+      if (response.data.length > 0) {
+        Alert.alert('Error', 'Email already exists');
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Error', 'An error occurred while checking email');
+      return;
+    }
+    try {
+      const response = await axios.post(`STRAPI`, {
+        username: email,
+        email,
+        password,
+        firstName,
+        lastName
+      });
+
+      if (response.status === 200) {
+        Alert.alert('Success', 'Registration successful');
+        navigation.navigate('LoginPage');
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Error', 'An error occurred while registering');
+    }
+  };return (
+    <View>
+      
+      <Text>First Name</Text>
+      <TextInput value={firstName} onChangeText={setFirstName} />
+
+      <Text>Last Name</Text>
+      <TextInput value={lastName} onChangeText={setLastName} />
+
+      <Text>Email</Text>
+      <TextInput value={email} onChangeText={setEmail} />
+
+      <Text>Password</Text>
+      <TextInput value={password} onChangeText={setPassword} secureTextEntry={true} />
+
+      <Text>Confirm Password</Text>
+      <TextInput value={confirmPassword} onChangeText={setConfirmPassword} secureTextEntry={true} />
+
+      <TouchableOpacity onPress={handleRegister}>
+        <Text>Create Account</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default FormPage;
+  
