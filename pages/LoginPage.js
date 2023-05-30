47b43fedbd16f0cd71f920995af235ab4e1b2534
devilman97 <118475791+devilman97@users.noreply.github.com>
2023-05-30 19:51:01 +0200

    LoginPage.js

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import axios from 'axios';
 
-const LoginPage = ({ setActiveTab }) => {
-  const handleLogin = () => {
-    setActiveTab(1); // Redirige vers la page du profil après la connexion
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      
+      const response = await axios.post('http://votre-api-strapi.com/auth/local', {
+        identifier: email,
+        password: password,
+      });
+
+      
+      console.log(response.data); 
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Login Page</Text>
-      <Button title="Login" onPress={handleLogin} />
+    <View style={styles.container}>
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={text => setEmail(text)}
+        style={styles.input}
+      />
+      <TextInput
+        label="Mot de passe"
+        value={password}
+        onChangeText={text => setPassword(text)}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button mode="contained" onPress={handleLogin}>
+        Se connecter
+      </Button>
     </View>
   );
 };
 
-export default LoginPage;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+  },
+  input: {
+    marginBottom: 16,
+  },
+});
+
+export default LoginScreen;
